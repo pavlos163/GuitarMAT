895468afcae0980e8ca95502a8acb76cc942b753
pk1914 <pavlos163@gmail.com>
2017-05-18 19:49:32 +0100

    Detecting duration, refactored code.

--- guitartab/mir.py
+++ guitartab/mir.py
@@ -8,24 +8,29 @@ from music21 import *
 from frequency_estimator import freq_from_hps
 
 def transcribe(filename):
-  y, sr = librosa.load(filename, sr=40000)
+  sr = 40000
+
+  y, sr = librosa.load(filename, sr=sr)
 
   # High-pass filter to remove low frequency noise.
   y = highpass_filter(y, sr)
 
   D = librosa.stft(y)
   fmin = 75
-  fmax = 1400
+  fmax = 1800
 
-  pitches = detect_pitch(y, sr)
+  pitches = detect_pitch(y, sr, 'stft')
 
-  print freq_from_hps(y, sr)
+  # slices = segment_sigal(y, sr)
+
+  # for segment in slices:
+  #   print freq_from_hps(segment, sr)
 
   # TODO: Create MusicXML file.
   notes = convert_to_notes(pitches)
 
-  # plot.plot_waveform(y)
-  # plot.plot_spectrogram(D, sr=sr, fmin=fmin, fmax=fmax)
+  # plot.plot_waveform(y=y, sr=sr)
+  # plot.plot_spectrogram(D, sr=sr)
   plt.close('all')
 
   return pitches
@@ -39,21 +44,35 @@ def convert_to_notes(pitches):
   return notes
 
 # Checking the pitch some frames the onset time increased precision.
-def detect_pitch(y, sr, onset_offset=5, fmin=75, fmax=1400):
-  onset_frames = librosa.onset.onset_detect(y=y, sr=sr)
-  
-  filtered_onset_frames = remove_dense_onsets(onset_frames)
-
-  pitches, magnitudes = librosa.piptrack(y=y, sr=sr, fmin=fmin, fmax=fmax)
+def detect_pitch(y, sr, method='stft', onset_offset=5, fmin=75, fmax=1400):
+  onset_frames = get_onset_frames(y, sr)
 
   notes = []
 
-  for i in range(0, len(filtered_onset_frames)):
-    onset = filtered_onset_frames[i] + onset_offset
-    index = magnitudes[:, onset].argmax()
-    pitch = pitches[index, onset]
-    if (pitch != 0):
-      notes.append(librosa.hz_to_note(pitch))
+  if method == 'stft':
+      pitches, magnitudes = librosa.piptrack(y=y, 
+        sr=sr, fmin=fmin, fmax=fmax)
+
+      for i in range(0, len(onset_frames)):
+        onset = onset_frames[i] + onset_offset
+        index = magnitudes[:, onset].argmax()
+        pitch = pitches[index, onset]
+        duration = detect_duration(magnitudes, index, onset)
+        if (pitch != 0):
+          notes.append(librosa.hz_to_note(pitch))
+  elif method == 'hps':
+    notes = []
+    # Segment signal into [ONSET + ONSET_OFFSET : ONSET + ONSET_OFFSET + X]
+    # For each segment, determine pitch with freq_from_hps()
+    # TODO.
+  else:
+    # Get STFT peaks and determine best candidate.
+    # TODO.
+    pitches, magnitudes = librosa.piptrack(y=y, sr=sr, fmin=fmin, fmax=fmax)
+    peaks = get_peaks(pitches, magnitudes, filtered_onset_frames)
+    for time_frame in peaks:
+      print time_frame
+      print all(peak >= 142 for peak in remove_values_from_list(time_frame, 0))
 
   return notes
 
@@ -75,8 +94,8 @@ def get_peaks(pitches, magnitudes, onset_frames, n=5):
 
 def highpass_filter(y, sr):
   # Parameters
-  #time_start = 0  # seconds
-  #time_end = 1  # seconds
+  # time_start = 0  # seconds
+  # time_end = 1  # seconds
   filter_stop_freq = 70  # Hz
   filter_pass_freq = 100  # Hz
   filter_order = 1001
@@ -99,7 +118,7 @@ def highpass_filter(y, sr):
 
 def segment_sigal(y, sr, onset_frames=None):
   if (onset_frames == None):
-    onset_frames = librosa.onset.onset_detect(y=y, sr=sr)
+    onset_frames = remove_dense_onsets(librosa.onset.onset_detect(y=y, sr=sr))
 
   oenv = librosa.onset.onset_strength(y=y, sr=sr)
   onset_bt = librosa.onset.onset_backtrack(onset_frames, oenv)
@@ -110,6 +129,11 @@ def segment_sigal(y, sr, onset_frames=None):
   
   return slices
 
+def get_onset_frames(y, sr):
+  onset_frames = librosa.onset.onset_detect(y=y, sr=sr)
+
+  return remove_dense_onsets(onset_frames)
+
 def remove_dense_onsets(onset_frames):
   indices_to_remove = []
 
@@ -119,3 +143,16 @@ def remove_dense_onsets(onset_frames):
       indices_to_remove.append(i)
 
   return np.delete(onset_frames, indices_to_remove)
+
+def remove_values_from_list(l, val):
+  return [value for value in l if value != val]
+
+def detect_duration(magnitudes, bin, time_frame):
+  # TODO: This is going to get messy if there is a new note played before
+  # the last note has finished.
+  duration = 0
+  while magnitudes[bin, time_frame] > 1:
+    duration += 1
+    time_frame += 1
+
+  return duration
--- guitartab/plot.py
+++ guitartab/plot.py
@@ -1,6 +1,6 @@
 import librosa.display
 import numpy as np
-import matplotlib.pylab as plt
+import matplotlib.pyplot as plt
 
 def plot_waveform(vector):
   plt.figure()
@@ -11,7 +11,7 @@ def plot_waveform(vector):
   plt.tight_layout()
   plt.savefig('static/plots/' + 'audio.png')
 
-def plot_spectrogram(stft_output, xlabel='Time', ylabel='Hz', sr=22050, fmin=None, fmax=None):
+def plot_spectrogram(stft_output, xlabel='Time', ylabel='Hz', sr=40000, fmin=None, fmax=None):
   plt.figure()
   librosa.display.specshow(librosa.amplitude_to_db(stft_output, 
     ref=np.max), y_axis='log', x_axis='time', sr=sr, fmin=fmin, fmax=fmax)
